a8b46a68e1b0b956513f764432017b43731c1e25
ThestralWarrior <38424304+ThestralWarrior@users.noreply.github.com>
2025-10-31 00:46:42 +0530

    scripts: additional debug parameters
    Added extra debug parameters -dDEBUG and -dPCLXLDEBUG to gpcl6.
    
    Signed-off-by: Ashirvad Mohanty <ashirvadm04@gmail.com>

--- buildandrun.sh
+++ buildandrun.sh
@@ -2,5 +2,5 @@
 
 make
 ./openrasterdriver < samples/Free_Test_Data_100KB_PDF.ras
-gpcl6 -sDEVICE=pdfwrite -sOutputFile=output.pdf -dNOPAUSE -dBATCH output.pcl
+gpcl6 -dDEBUG -dPCLXLDEBUG -sDEVICE=pdfwrite -sOutputFile=output.pdf -dNOPAUSE -dBATCH output.pcl
 
