b20afd55474925a4b048d0d736c6b836eb1715e5
ThestralWarrior <38424304+ThestralWarrior@users.noreply.github.com>
2025-11-04 21:21:39 +0530

    feat: support for different orientations, media, etc
    Added detection capability for the page type to be used for printing on,
    support for different orientation types, and color depth mapping. Corrected
    initial cursor positioning rather than starting from the top-left corner.
    
    Signed-off-by: Ashirvad Mohanty <ashirvadm04@gmail.com>

--- pcl6.c
+++ pcl6.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <string.h>
+#include <math.h>
 #include "pcl6.h"
 
 #define NOT_BLANK 1
@@ -8,6 +9,39 @@
 #define PUT8(v)		fputc((v) & 0xFF, out)
 #define PUT16(v)	do { fputc((v) & 0xFF, out); fputc(((v) >> 8) & 0xFF, out); } while(0)
 #define PUT32(v)	do { fputc((v) & 0xFF, out); fputc(((v) >> 8) & 0xFF, out); fputc(((v) >> 16) & 0xFF, out); fputc(((v) >> 24) & 0xFF, out); } while(0)
+
+static uint8_t pcl6_return_media(cups_page_header2_t *header) {
+	float w = header->cupsPageSize[0];
+	float h = header->cupsPageSize[1];
+	float eps = 5.0f;
+
+	uint8_t media = PCL_E_MEDIA_A4;
+
+	if(fabs(w - 612.0f) < eps && fabs(h - 792.0f) < eps)
+		media = PCL_E_MEDIA_LETTER;
+    
+	if(fabs(w - 612.0f) < eps && fabs(h - 1008.0f) < eps)
+        media = PCL_E_MEDIA_LEGAL;
+    
+	if(fabs(w - 595.0f) < eps && fabs(h - 842.0f) < eps)
+        media = PCL_E_MEDIA_A4;
+    
+	if(fabs(w - 522.0f) < eps && fabs(h - 756.0f) < eps)
+        media = PCL_E_MEDIA_EXEC;
+    
+	if(fabs(w - 792.0f) < eps && fabs(h - 1224.0f) < eps)
+        media = PCL_E_MEDIA_LEDGER;
+    
+	if(fabs(w - 842.0f) < eps && fabs(h - 1191.0f) < eps)
+        media = PCL_E_MEDIA_A3;
+    
+	if(fabs(w - 420.0f) < eps && fabs(h - 595.0f) < eps)
+        media = PCL_E_MEDIA_A5;
+
+	printf("Media: %x\n", media);
+
+    return media;
+}
 
 /* Helper to emit attribute (ubyte) */
 static void pcl6_emit_attr_ubyte(FILE *out, uint8_t attr_id, uint8_t value) {
@@ -35,6 +69,7 @@ static void pcl6_emit_attr_uint16_xy(FILE *out, uint8_t attr_id, uint16_t x, uin
 }
 
 void pcl6_start_session(FILE *out, cups_page_header2_t *header) {
+	/* BeginSession */
 	fputs(PCL_HEADER, out);
     pcl6_emit_attr_ubyte(out, PCL_ATTR_MEASURE, PCL_E_MEASURE_INCH);
     pcl6_emit_attr_uint16_xy(out, PCL_ATTR_UNITS_PER_MEASURE, header->HWResolution[0], header->HWResolution[1]);
@@ -43,9 +78,30 @@ void pcl6_start_session(FILE *out, cups_page_header2_t *header) {
 }
 
 void pcl6_start_job(FILE *out, cups_page_header2_t *header) {
+	uint8_t orientation = 0;
+
+	switch(header->Orientation) {
+		case CUPS_ORIENT_0:
+			orientation = PCL_E_ORIENT_PORTRAIT;
+			break;
+		case CUPS_ORIENT_90:
+			orientation = PCL_E_ORIENT_LANDSCAPE;
+			break;
+		case CUPS_ORIENT_180:
+			orientation = PCL_E_ORIENT_REVERSE_PORTRAIT;
+			break;
+		case CUPS_ORIENT_270:
+			orientation = PCL_E_ORIENT_REVERSE_LANDSCAPE;
+			break;
+		default:
+			orientation = PCL_E_ORIENT_PORTRAIT;
+	}
+
+	uint8_t media = pcl6_return_media(header);
+
 	/* BeginPage */
-	pcl6_emit_attr_ubyte(out, PCL_ATTR_MEDIA_SIZE, PCL_E_MEDIA_A4);
-	pcl6_emit_attr_ubyte(out, PCL_ATTR_ORIENTATION, PCL_E_ORIENT_PORTRAIT);
+	pcl6_emit_attr_ubyte(out, PCL_ATTR_MEDIA_SIZE, media);
+	pcl6_emit_attr_ubyte(out, PCL_ATTR_ORIENTATION, orientation);
 	PUT8(PCL_OP_BEGIN_PAGE);
 
 	pcl6_emit_attr_ubyte(out, PCL_ATTR_SOURCE_TYPE, PCL_E_DEFAULT_SOURCE);
@@ -53,17 +109,35 @@ void pcl6_start_job(FILE *out, cups_page_header2_t *header) {
 	PUT8(PCL_OP_OPEN_DATA_SRC);
 
 #if NOT_BLANK
+	int offsetX = (int)(header->Margins[0] * header->HWResolution[0] / 72.0);
+	int offsetY = (int)(header->Margins[1] * header->HWResolution[1] / 72.0);
+
 	/* SetCursor */
 	PUT8(PCL_DT_UINT16_XY);
-	PUT16(0);
-	PUT16(0);
+	PUT16(offsetX);
+	PUT16(offsetY);
 	PUT8(PCL_ATTR_TAG_UBYTE);
 	PUT8(PCL_ATTR_POINT);
 	PUT8(PCL_OP_SET_CURSOR);
 	
+	uint8_t colorDepth = 0;
+	switch (header->cupsBitsPerColor) {
+		case 1:
+			colorDepth = PCL_E_1BIT;
+			break;
+		case 4:
+			colorDepth = PCL_E_4BIT;
+			break;
+		case 8:
+			colorDepth = PCL_E_8BIT;
+			break;
+		default:
+			colorDepth = PCL_E_8BIT;
+	}
+
 	/* BeginImage */
 	pcl6_emit_attr_ubyte(out, PCL_ATTR_COLOR_MAPPING, PCL_E_DIRECT_PIXEL);
-	pcl6_emit_attr_ubyte(out, PCL_ATTR_COLOR_DEPTH, PCL_E_8BIT);
+	pcl6_emit_attr_ubyte(out, PCL_ATTR_COLOR_DEPTH, colorDepth);
 	pcl6_emit_attr_uint16(out, PCL_ATTR_SOURCE_WIDTH, header->cupsWidth);
 	pcl6_emit_attr_uint16(out, PCL_ATTR_SOURCE_HEIGHT, header->cupsHeight);
 	pcl6_emit_attr_uint16_xy(out, PCL_ATTR_DESTINATION_SIZE, header->cupsWidth, header->cupsHeight);
